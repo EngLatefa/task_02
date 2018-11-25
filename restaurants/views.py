c518281703dac7b0508b9a126ad97abeaee14d1d
Latefs <latefaalhassan@hotmail.com>
2018-11-25 18:26:10 +0000

    challenge

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def template(request):
+	context = {"msg": "Hello World!"}
+	return render(request, 'untitled.html', context)
